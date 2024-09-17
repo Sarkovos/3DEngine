82c3b206cbd3ed78567463d55fae4ad0e38ded55
Sarkovos <mastersarkovos@gmail.com>
2024-09-17 04:00:29 -0500

    bresensam i ham

--- src/display.c
+++ src/display.c
@@ -88,6 +88,10 @@ void draw_line(int x0, int y0, int x1, int y1, uint32_t color) {
 }
 
 //Bresenhams algorithm
+//As the algorithm plots in the x direction, error will increase in the positive
+//direction eventually making e2 greater than dx, where it will stop plotting in the
+//x irection. As it goes in the y direction, error will increment in the negative direction,
+//eventually making e2 less than dy.
 void bres_draw_line(int x0, int y0, int x1, int y1, uint32_t color)
 {
 
